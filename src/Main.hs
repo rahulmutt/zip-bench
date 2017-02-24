706746b8e8c39541707aa953534fb93a98646f6f
Rahul Muttineni <rahulmutt@gmail.com>
2017-02-24 11:51:40 +0530

    Zip64 bug

--- src/Main.hs
+++ src/Main.hs
@@ -6,12 +6,22 @@ import Codec.Archive.Zip
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Monad.Catch (MonadCatch(..), MonadThrow)
 import Data.Map.Lazy (keys)
+import qualified Data.ByteString as BS
 import System.Directory
 import System.Environment
 import Control.Monad
 
+createLargeZip :: String -> Int -> IO ()
+createLargeZip name size = do
+  p <- parseRelFile name
+  ps <- mapM (parseRelFile . ('a':) . show) range
+  createArchive p $
+    forM_ ps $ \p -> do
+      es <- mkEntrySelector p
+      addEntry Deflate (BS.pack [1]) es
+  where range = [1..size]
+
 main :: IO ()
 main = do
-  p <- parseRelFile "Tuple.jar"
-  entries <- withArchive p $ keys <$> getEntries
-  mapM_ print entries
+  createLargeZip "Before.jar" 65534
+  createLargeZip "After.jar" 65535
